4d31c5bef894ad5767fbc284bf0a0867e6e7bb2c
Your Name <sravankumarboini005.com>
2025-04-02 14:01:39 +0530

    pythn program added

---
+++ solutions/min_time_to_visit_all_points.py
@@ -0,0 +1,7 @@
+def min_time_to_visit_all_points(points):
+    time = 0
+    for i in range(1, len(points)):
+        dx = abs(points[i][0] - points[i - 1][0])
+        dy = abs(points[i][1] - points[i - 1][1])
+        time += max(dx, dy)
+    return time
---
+++ testcases/test.py
@@ -0,0 +1,25 @@
+from minTimeToVisitAllPoints import min_time_to_visit_all_points
+
+def test_case(points, expected, test_number, passed, failed):
+    result = min_time_to_visit_all_points(points)
+    print(f"Test Case {test_number}: Expected = {expected}, Actual = {result}")
+
+    if result == expected:
+        print(f"Test Case {test_number} PASS")
+        passed[0] += 1
+    else:
+        print(f"Test Case {test_number} FAIL")
+        failed[0] += 1
+
+def run_tests():
+    passed, failed = [0], [0]
+
+    test_case([[0, 0]], 0, 1, passed, failed)
+    test_case([[0, 0], [1000, 1000]], 1000, 2, passed, failed)
+    test_case([[0, 0], [5, 0], [10, 0]], 10, 3, passed, failed)
+
+    print(f"\nTest cases passed: {passed[0]}")
+    print(f"Test cases failed: {failed[0]}")
+
+if __name__ == "__main__":
+    run_tests()
