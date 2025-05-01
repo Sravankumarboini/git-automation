f2ffebf14712996cee0fed6d80826ea0b70f6b31
Sravankumarboini <sravankumarboini005@gmail.com>
2025-05-01 12:45:21 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -5,6 +5,7 @@
 int minTimeToVisitAllPoints(int** points, int pointsSize, int* pointsColSize) {
     //code here 
     int totalTime = 0;
+    
     for (int i = 0; i < pointsSize - 1; ++i) {
         int xDiff = abs(points[i+1][0] - points[i][0]);
         int yDiff = abs(points[i+1][1] - points[i][1]);
